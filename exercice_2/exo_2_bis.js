b58123a5a53491b29eab6ab9330ce7dfb207dbfb
Ana <analutzky@gmail.com>
2016-05-07 00:25:06 +0200

    Travail de la semaine 9

---
+++ exercice_2/exo_2_bis.js
@@ -0,0 +1,30 @@
+$liste = document.getElementById("liste");
+$texte = document.getElementById("texte");
+$ajouter = document.getElementById("ajouter");
+$annuler = document.getElementById("annuler");
+
+tableau = ["Bob", "Julien", "Roger"];
+
+function afficherListe() {
+    elements = "";
+    for (i = 0; i < tableau.length; i++) {
+        elements += "<li>" + tableau[i] + "</li>";
+    }
+    $liste.innerHTML = elements;
+}
+
+function ajouter() {
+    tableau.push($texte.value);
+    afficherListe();
+}
+
+$ajouter.onclick = ajouter;
+
+function annuler() {
+	tableau.pop();
+	afficherListe();
+}
+
+$annuler.onclick = annuler;
+
+afficherListe();
